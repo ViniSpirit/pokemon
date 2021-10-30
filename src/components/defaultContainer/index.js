3155081ecb55a7f9d6329b53ce7fc90b45d4f6fd
Marcos Vinicius <marcosvims@gmail.com>
2021-10-30 08:58:13 -0300

    ajustes

--- src/components/defaultContainer/index.js
+++ src/components/defaultContainer/index.js
@@ -1,10 +1,10 @@
-import styles from "./defaultImage.module.css"
+import styles from "./defaultContainer.module.css"
 import Image from "next/image"
 import defaultImage from "../../images/defaultImage.png"
 
 function DefaultContainer() {
   return (
-    <div className={styles.defaultImage}>
+    <div className={styles.defaultContainer}>
       <Image src={defaultImage} width={200} height={200} />
     </div>
   )
